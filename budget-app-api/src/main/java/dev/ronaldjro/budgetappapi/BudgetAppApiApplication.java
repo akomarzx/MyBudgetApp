ebaa5a96b53fad0a0ed17d2348a2351c2b0e1663
Ronald Ombao <ronaldjr.ombao@gmail.com>
2026-09-20 18:56:56 -0400

    Budget App Initial Commit

---
+++ budget-app-api/src/main/java/dev/ronaldjro/budgetappapi/BudgetAppApiApplication.java
@@ -0,0 +1,13 @@
+package dev.ronaldjro.budgetappapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetAppApiApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(BudgetAppApiApplication.class, args);
+    }
+
+}
---
+++ budget-app-api/src/test/java/dev/ronaldjro/budgetappapi/BudgetAppApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ronaldjro.budgetappapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetAppApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
